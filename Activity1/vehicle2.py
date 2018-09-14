f7cf5e575cd531ee60bc1f47ab95f04a2cc57754
XinyuYang <xyang2@macalester.edu>
2018-09-14 14:57:01 -0500

    Braitenberg 2

---
+++ Activity1/vehicle2.py
@@ -0,0 +1,34 @@
+import ev3dev.ev3 as ev3
+from basicBot import BasicBot
+
+myBot = BasicBot("Loki")
+myBot.setGyroSensor('in1')
+myBot.setColorSensor('in3')
+myBot.setUltrasonicSensor('in4')
+
+# myBot.setMotorPort('leftMotor', 'outB')
+# myBot.setMotorPort('rightMotor', 'outD')
+
+def convertDistToSpeed(distance):
+    if distance > 25:
+        return 1
+    else:
+        return (distance - 25) / 25.0
+
+def readLeftAndRight(robot):
+    robot.turnLeft(0.2, 0.2)
+    leftVal = robot.readUltra()
+    robot.turnRight(0.2, 0.4)
+    rightVal = robot.readUltra()
+    robot.turnLeft(0.2, 0.2)
+    return leftVal, rightVal
+
+
+bttn = ev3.Button()
+while not bttn.any():
+    leftDist, rightDist = readLeftAndRight(myBot)
+    leftSpeed = convertDistToSpeed(leftDist)
+    rightSpeed = convertDistToSpeed(rightDist)
+    myBot.curve(leftSpeed, rightSpeed)
+
+myBot.stop()
